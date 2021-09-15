7fa5bc6a7d25fae6bd84feee5d5fc005b9a233bd
Arnie97 <arnie97@gmail.com>
2021-09-15 16:03:08 +0800

    Refactor: remove main function

--- felinizer.hs
+++ felinizer.hs
@@ -1,21 +1,19 @@
 #!/usr/bin/env runhaskell
 
 import Data.Char (isAlpha, isPunctuation, isSpace)
-import Data.List (dropWhile, dropWhileEnd)
+import Data.List (dropWhile, dropWhileEnd, isPrefixOf)
 import Data.List.Split (splitOn)
 import System.Environment (getArgs)
 
-main = interact func
-    where func = unlines .
-                 map (nth 0) .
-                 filter beijing .
-                 filter mobilePhone .
-                 fmap (fmap trim . splitOn ";") .
-                 lines
+main = interact $
+    unlines .
+    fmap (\line -> line !! 0) .
+    filter (numberLocation (isPrefixOf "北京市")) .
+    filter (numberType ((==) "MOBILE")) .
+    fmap (fmap trim . splitOn ";") .
+    lines
+    where numberLocation cond line = cond $ last line
+          numberType     cond line = cond $ line !! 2
 
-nth n xs = xs !! n
 trim = dropWhileEnd shouldTrim . dropWhile shouldTrim
     where shouldTrim s = isSpace s || s == '"'
-
-beijing line = last line == "北京市"
-mobilePhone line = line!!2 == "MOBILE"
