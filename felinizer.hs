e8fb323e9506aba18566f9ad03e6dfbef47f6abd
Arnie97 <arnie97@gmail.com>
2021-09-16 08:43:06 +0800

    expand

--- felinizer.hs
+++ felinizer.hs
@@ -1,13 +1,13 @@
 #!/usr/bin/env runhaskell
 
-import Data.Char (isAlpha, isPunctuation, isSpace)
+import Data.Char (chr, ord, isAlpha, isPunctuation, isSpace)
 import Data.List (dropWhile, dropWhileEnd, isPrefixOf)
 import Data.List.Split (splitOn)
 import System.Environment (getArgs)
 
 main = interact $
     unlines .
-    fmap (\line -> line !! 0) .
+    fmap (\line -> felinize [] False [] 0 (line !! 0)) .
     filter (numberLocation (isPrefixOf "北京市")) .
     filter (numberType ((==) "MOBILE")) .
     fmap (fmap trim . splitOn ";") .
@@ -15,5 +15,16 @@ main = interact $
     where numberLocation cond line = cond $ last line
           numberType     cond line = cond $ line !! 2
 
+-- >>> trim "  \"hello world\"  \n"
+-- "hello world"
 trim = dropWhileEnd shouldTrim . dropWhile shouldTrim
     where shouldTrim s = isSpace s || s == '"'
+
+-- >>> expand [] "PN-LGF-CA"
+-- "ACDEFGLMNP"
+expand :: String -> String -> String
+expand (y:ys) ('-':x:xs)
+    | x == y    = expand (y:ys) xs
+    | otherwise = expand (chr (ord y - 1):y:ys) ('-':x:xs)
+expand ys (x:xs) = expand (x:ys) xs
+expand ys [] = ys
